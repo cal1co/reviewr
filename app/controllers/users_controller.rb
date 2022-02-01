757b36a80e46a3355f8c3d41a954b8f0bfeaff9e
Alex King <calicoalex@AlexnoMacBook-Pro.local>
2022-02-01 11:17:24 +1100

    heroku 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,38 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
+
+
   def create
-    @user = User.create user_params
     user_em = User.find_by email: params[:email]
-    user_name = User.find_by username: params[:username]
-    # if user_name.present? 
-      # raise 'hell'
+    user_name = User.where username: params[:username]
+    @user = User.create user_params
+    # raise 'hell'
+    
+    if @user.valid?
       session[:user_id] = @user.id
       redirect_to home_path
-    # end
+      
+    else
+      flash[:error] = 'Invalid email or password'
+      redirect_to new_user_path
 
-  end
+    end # create() if/else
+
+  end #create()
 
   def destroy
   end
 
+
+  def show
+    @user = @current_user
+  end
+
+  private
   def user_params
-    params.require(:user).permit(:username, :image, :email, :password)
+     params.require(:user).permit(:username, :image, :email, :password)
   end
 
 end
